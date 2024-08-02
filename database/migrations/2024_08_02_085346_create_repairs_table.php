7a6b742c92d924ad5ad5c9e12a8a0d5cbea44d2a
Maxime Bouger <bougermaxime@gmail.com>
2024-08-02 11:06:04 +0200

    Initial commit

---
+++ database/migrations/2024_08_02_085346_create_repairs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('repairs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('car_id');
+            $table->foreign('car_id')->references('id')->on('cars');
+            $table->unsignedBigInteger('repair_type_id');
+            $table->foreign('repair_type_id')->references('id')->on('repair_types');
+            $table->float('price')->nullable();
+            $table->date('date')->nullable();
+            $table->boolean('is_planned_repair')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('repairs');
+    }
+};
