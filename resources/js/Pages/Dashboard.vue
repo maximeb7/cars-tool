acf59f609049c8c33adc401993a2281d60df1835
Maxime Bouger <bougermaxime@gmail.com>
2024-09-05 17:29:40 +0200

    [FRONT-END] Add Vuetify Library. Start to populate Dashboard component

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,32 +3,34 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import { onMounted, ref } from 'vue';
 import getUserInformations from '@/Services/User/getUserInformations.js';
-;
 
 // Définir une variable pour stocker les informations de l'utilisateur
 const userInformations = ref(null);
+const carsAndRepairs = ref([])
 
 onMounted(async () => {
     const userId = localStorage.getItem('userId');
 
     if (userId) {
-        console.log(userId, 'User ID found');
-
-        try {
-            // Appel au service pour obtenir les informations de l'utilisateur
-            const data = await getUserInformations(userId);
-
-            // Stocker les données récupérées dans la variable
-            userInformations.value = data;
-            console.log(data);
-
-            // Optionnel: Stocker les données dans le localStorage
-            localStorage.setItem("userInfos", JSON.stringify(data));
-        } catch (error) {
-            console.error("Erreur lors de la récupération des informations de l'utilisateur", error);
-        }
+        await fetchUserInformations(userId);
     }
 });
+
+const fetchUserInformations = async (userId) => {
+    try {
+        const data = await getUserInformations(userId);
+        userInformations.value = data;
+
+        if (data.cars.length > 0) {
+            carsAndRepairs.value = data.cars;
+        }
+
+        localStorage.setItem("userInfos", JSON.stringify(data));
+    } catch (error) {
+        console.error("Erreur lors de la récupération des informations de l'utilisateur", error);
+    }
+};
+
 </script>
 
 <template>
@@ -44,11 +46,49 @@ onMounted(async () => {
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         Bienvenue
-                        <!-- Affiche les informations de l'utilisateur -->
-                        <pre v-if="userInformations">{{ userInformations }}</pre>
                     </div>
                 </div>
             </div>
         </div>
+        <div  v-if="carsAndRepairs.length > 0" class="py-3">
+            <div class="max-w-10xl mx-auto sm:px-6 lg:px-8">
+                <v-row class="mt-5" v-for="car in carsAndRepairs">
+                    <v-col cols="12" md="4">
+                        <v-card rounded="xl" elevation="1" >
+                            <v-card-title> {{ car.brandName }} {{car.model}}</v-card-title>
+                            <v-img
+                                border="opacity-50 sm"
+                                max-width="mx-auto"
+                                rounded="sm"
+
+                                class="ma-5"
+                                alt="MyCarPicture"
+                                src="/images/OpelAstra.jpg"
+                                :width="410"
+                                aspect-ratio="16/9"
+                                cover
+                            ></v-img>
+                            <v-card-subtitle> Année {{ car.year}}</v-card-subtitle>
+                            <v-card-text> Nombre de factures:  {{ car.repairs.length > 0 ? car.repairs.length : 0 }}</v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </div>
+        </div>
+        <div  v-else  class="py-3">
+            <div class="max-w-10xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <h2>Pas de véhicules enregistrées pour le momment</h2>
+                        <p>Ajouter en une :</p>
+                        <v-btn >
+                            Ajouter une voiture
+                        </v-btn>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </AuthenticatedLayout>
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,24 @@
 import './bootstrap';
 import '../css/app.css';
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -15,6 +27,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(vuetify)
             .mount(el);
     },
     progress: {
